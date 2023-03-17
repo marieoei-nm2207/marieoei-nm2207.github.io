a032eec4147523db324bf004e3ae6060f014f2db
marieoei-nm2207 <125446400+marieoei-nm2207@users.noreply.github.com>
2023-03-17 09:15:23 +0800

    170323 updated js with game 2

--- appscripts/main.js
+++ appscripts/main.js
@@ -7,7 +7,7 @@ let buttonContainer = document.getElementById("buttonContainer");
 // button variables
 let yesButton = document.createElement("button");
 let noButton = document.createElement("button");
-let theEndButton = document.createElement("button");
+let endButton = document.createElement("button");
 
 // game button variables
 let game1 = document.createElement("button");
@@ -16,9 +16,9 @@ let game3 = document.createElement("button");
 game1.className = "gbutton";
 game2.className = "gbutton";
 game3.className = "gbutton";
-game1.innerHTML = "Game 1";
+game1.innerHTML = "U/C";
 game2.innerHTML = "Game 2";
-game3.innerHTML = "Game 3";
+game3.innerHTML = "U/C";
 
 // home locations variables
 let home1 = document.createElement("button"); // Moonlit Marshes - alert "You either got the wrong home or the wrong code!"
@@ -95,30 +95,23 @@ function noButtonClicked() {
     yesButton.remove();
     noButton.remove();
 
-    theEndButton.className = "button";
-    theEndButton.id = "buttonToHome";
-    theEndButton.innerHTML = "Leave";
-    theEndButton.onclick = function () {
-        goBack();
-        /* console.log("theEndButton is clicked"); */
-      };
-    buttonContainer.appendChild(theEndButton);
-}
+    endButton.className = "button";
+    endButton.id = "buttonToHome";
+    endButton.innerHTML = "Leave";
 
-// creating the ending page
-function goBack() {    
-    changeText.removeEventListener("click", function(){
-        console.log('hi');
+    endButton.addEventListener('click', function(){
+        intText.innerHTML = "Goodbye, Adventurer.";
+        endButton.remove();
+
+        let plsRef = document.createElement("p");
+        plsRef.id = "click2Cont";
+        plsRef.innerHTML = "Please refresh the page if you want to try again.";
+        lowerText.appendChild(plsRef);
+
+        gameState = 1;
+        /* console.log("endButton is clicked"); */
     });
-    intText.innerHTML = "Goodbye, Adventurer.";
-    theEndButton.remove();
-
-    let plsRef = document.createElement("p");
-    plsRef.id = "click2Cont";
-    plsRef.innerHTML = "Please refresh the page if you want to try again.";
-    lowerText.appendChild(plsRef);
-
-    gameState = 1;
+    buttonContainer.appendChild(endButton);
 }
 
 // click the yes button
@@ -213,8 +206,10 @@ document.body.addEventListener('keydown', function(e){
     }
 });
 document.body.addEventListener('keyup', function(e){
-    noteMessage.remove();
-    note.remove();
+    if (e.keyCode == 191) {
+        noteMessage.remove();
+        note.remove();
+    }
 });
 
 // implementing the game buttons
@@ -257,7 +252,11 @@ let listen = document.createElement("button");
 game2.addEventListener('click', function(){
     gameState = 7;
 
-    mainPiano.style.display = "block";
+    content.appendChild(listen);
+    listen.style.marginBottom = "-30px";
+    mainPiano.style.marginBottom = "0";
+    mainPiano.style.display = "block"; 
+    content.appendChild(mainPiano);
 
     game1.remove();
     game2.remove();
@@ -266,9 +265,7 @@ game2.addEventListener('click', function(){
     intText.innerHTML = "Click on 'Listen' to listen to a short tune." + "<br>" + "Imitate the tune correctly to get a clue and a part of the code.",
     intText.style.fontSize = "18px";
     click2Cont.innerHTML = "If you press 5 keys wrongly, there will be a reset.";
-    
-    content.appendChild(listen);
-    
+       
     listen.addEventListener('click', function() {
         listenToTune();
     });
@@ -308,18 +305,17 @@ function keyTune() {
             
             // store the key code in the userKeys array
             userKeys.push(e.keyCode);
-        
+                        
             // compare the user's key presses to the correct sequence
-            if (userKeys.length === pianoAudio.length && userKeys.every((value, index) => value === pianoAudio[index])) {
-                // if the user's key presses match the correct sequence, show the clue and number            
+            if (userKeys.length === pianoAudio.length && userKeys.every((value, index) => value === pianoAudio[index])) {           
                 
                 mainPiano.style.display = "none";
                 listen.remove();
 
                 intText.innerHTML = "Here's your clue and number.";
+                intText.style.fontSize = "36px";
                 click2Cont.innerHTML = "Click to continue.";
-
-                gameState == 7.5;
+                
                 clue2();
 
             } else if (userKeys.length === pianoAudio.length && userKeys.some((value, index) => value !== pianoAudio[index])) {
@@ -329,16 +325,26 @@ function keyTune() {
             }
         }
     });
-    function clue2() {
-        if (gameState == 7.5){
-            content.appendChild(note);
-            note.appendChild(noteMessage);
-            note.style.backgroundColor = "rgb(235, 205, 124)";
-            note.style.color = "rgb(130, 95, 0)";
-            noteMessage.innerHTML = "It looks cool;" + "<br>" + "<br>" + "2";
-            console.log("clue printed");
-        }
-    }
+}
+
+function clue2() {
+        // if the user's key presses match the correct sequence, show the clue and number 
+        content.appendChild(note);
+        note.appendChild(noteMessage);
+        note.style.backgroundColor = "rgb(51, 102, 14)";
+        note.style.color = "rgb(130, 95, 0)";
+        noteMessage.innerHTML = "It looks cool;" + "<br>" + "<br>" + "2";
+        noteMessage.style.textAlign = "center";
+        noteMessage.style.fontSize = "24px";
+        noteMessage.style.margin = "auto";
+
+        console.log("clue 2 printed");
+
+        gameState = 4;
+        clickBlob3();
+
+        game2.style.backgroundColor = "rgb(116, 140, 124)";
+        game2.style.color = "white";
 }
 
 function playNote(e){
@@ -357,6 +363,156 @@ function playNote(e){
 }
 
 // ------- GAME 3 - collect/sort out the berries
-// gameState = 8;
 // have a timer event, maybe 25 seconds; player has to either sort or collect 20 berries within the timing
 // when timer runs out and the game is uncompleted, reset
+// Fruit Catch Game: https://www.youtube.com/watch?v=sDlrNT0myIo, https://www.youtube.com/watch?v=UdSVPgNkOyU
+/* let startGame3 = document.createElement("button");
+startGame3.className = "button";
+startGame3.id = "g3StartButton";
+startGame3.innerHTML = "Start";
+
+let score = 0;
+
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+canvas.width = mainDiv.offsetWidth; // gets the width of mainDiv
+canvas.height = mainDiv.offsetHeight; // gets the height of mainDiv
+
+const berries = []; // creating multiple berries, setting to an empty array
+const basket = [];
+
+game3.addEventListener('click', function(){
+    gameState = 8;
+
+    game1.remove();
+    game2.remove();
+    game3.remove();
+
+    intText.innerHTML = blobName.value + " is hungry." + "<br>" + "Catch some berries for him but be sure to avoid the poisonous ones (Hint: They look a little dull).";
+    click2Cont.innerHTML = "Collect 30 berries in 25 seconds." + "<br>" + "If time runs out before your succeed, there will be a reset."
+                            + "Move your cursor to move the basket to collect the berries.";
+    click2Cont.style.marginBottom = "260px";
+    content.appendChild(startGame3);
+
+    startGame3.addEventListener('click', function(){
+        startGame3.remove();
+        intText.innerHTML = "";
+        click2Cont.innerHTML = "";
+
+
+    });
+    
+});
+
+const berryImgs = [
+    "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/26169d5a5745ad9.png", // red berry http://pixelartmaker.com/art/26169d5a5745ad9
+    "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d78af06eea21b17.png", // blueberry http://pixelartmaker.com/art/d78af06eea21b17
+    "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/7ded3a2e20e36f6.png", // orange raspberry http://pixelartmaker.com/art/7ded3a2e20e36f6
+    "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/eb71bf72598896e.png" // poison berry http://pixelartmaker.com/art/eb71bf72598896e
+]
+
+class Berry {
+    // position handler of the berry and its physical appearance
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+        this.image = new Image(60, 60);
+        this.image.src = berryImgs[Math.floor(Math.random() * berryImgs.length)]; // to randomly select an image source from the array
+
+        // adding the animation of the berries
+        this.yVel = 3; // velocity of the y-positioning (up/down)
+    }
+    
+    // making the berries
+    draw() {
+        c.beginPath();
+        c.drawImage(this.image, this.x - this.radius, this.y - this.radius, this.radius*2, this.radius*2);
+        c.closePath();
+    }
+
+    // managing the movements
+    update() {
+        this.draw();
+        this.y += this.yVel;
+    }
+}
+
+const basketImg = new Image(120, 120);
+basketImg.src = "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/09bbb262de794bb.png"; //http://pixelartmaker.com/art/09bbb262de794bb
+
+class Basket {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;   
+    }
+
+    draw() {
+        c.drawImage(basketImg, this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+    }
+}
+
+basket.push(new Basket(canvas.width/2 - 120/2, canvas.height - 100 - 30, 120, 100));
+
+// setting interval between each berry falling to 2 seconds
+setInterval(() => {
+    let berryXPos = Math.random() * ((canvas.width-60)-60)+60;
+    berries.push(new Berry(berryXPos, -10, 30));
+
+}, 500);
+
+// getting the berry to animate falling down
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height); // making it look like the berry is falling rather than stretching out
+
+    berries.forEach((berry, index) => {
+        berry.update();
+
+        // getting the berry to disappear upon hitting the end of the canvas/screen
+        if (berry.y + berry.radius > canvas.height) {
+            setTimeout(function(){
+                berries.splice(index, 1);
+            }, 0);
+            console.log("over");
+        }
+
+        // when berry hits the basket, the score increases
+        // calculating distance between the center of the berry and the center of the basket using pythagorean theorem;
+        // if dist < (berry radius + half the width of the basket), remove berry from berries array and increase the score by 1
+        const dx = berry.x - (basket[0].x + basket[0].width/2);
+        const dy = berry.y - (basket[0].y + basket[0].height/2);
+        const distance = Math.sqrt(dx*dx + dy*dy); 
+        if (distance < berry.radius + basket[0].width/2) {
+            berries.splice(index, 1);
+            score++;
+            console.log(score);
+
+            let scoreText = document.createElement("p");
+            mainDiv.appendChild(scoreText);
+            scoreText.id = "scoreText";
+            scoreText.innerHTML = score;
+        }
+    })
+
+    basket.forEach((bskt) => {
+        bskt.update();
+    })
+}
+animate();
+
+document.addEventListener('mousemove', function(e){
+    let x = e.clientX - canvas.offsetLeft; // subtract canvas offset to get relative position
+    basket[0].x = x - basket[0].width - 50; // update x-position of the center of the basket
+}); */
+
+
+// when all games have been played, show the options for home
+// check whether all games have been played using .style.backgroundColor = "rgb(116, 140, 124)"
